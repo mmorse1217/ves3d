da4312ceb8cfba5c52fd70ebd124b5ee6816f3c1
Abtin Rahimian <rahimian@gatech.edu>
2010-01-19 13:02:57 -0500

    arn

---
+++ docs/mainpage.h
@@ -0,0 +1,37 @@
+/**
+ * @file   mainpage.h
+ * @author Abtin Rahimian <abtin@romario>
+ * @date   Tue Jan 19 11:22:23 2010
+ * 
+ * @brief  The main page for the Ves3D package manual.
+ */
+
+
+/*!@mainpage A simple manual
+  
+  Some general info.
+  
+  This manual is divided in the following sections:
+  - @subpage Client
+  - @subpage Developer
+*/
+
+////////////////////////////////////////////////////////////
+
+/*!@page Client Clients' Manual
+  This page introduces the user to the topic.
+  Now you can proceed to the \ref advanced "advanced section".
+  
+  @section requiredPack Required Packages
+  @section Instal Installation
+ */
+
+////////////////////////////////////////////////////////////
+
+/*!@page Developer Developers' Documentation
+  This page is for advanced users.
+  Make sure you have first read \ref intro "the introduction".
+
+  @section shTran Spherical Harmonics Transform
+  @section diff Differentiation
+ */
