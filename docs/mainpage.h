68b7b636d65fba67010de21c26649724c6e0c3a1
Abtin Rahimian <rahimian@gatech.edu>
2010-01-25 15:28:24 -0500

    arn

--- docs/mainpage.h
+++ docs/mainpage.h
@@ -35,8 +35,8 @@
 
   - <b>Variable Names:</b> Variable names are all lowercase, with
   underscores between words. Class member variables have trailing
-  underscores. For instance, <tt>my_local_variable,
-  my_member_variable_.</tt>
+  underscores. For instance, <code>my_local_variable,
+  my_member_variable_.</code>
 
   - <b>Class Data Members:</b> Data members, also called instance
   variables or member variables, are treated like regular variable
@@ -47,24 +47,69 @@
   members in classes have.
 
   - <b>Global Variables:</b> They should be rare in any case, but if
-  you use one, prefix it with <tt>the_</tt> marker to easily
+  you use one, prefix it with <code>the_</code> marker to easily
   distinguish it from local variables.
 
   - <b>Constant Names:</b> All compile-time constants, whether they
   are declared locally, globally, or as part of a class, follow a
   slightly different naming convention from other variables. Use a
-  <tt>k_</tt> followed by words, like <tt>const int k_days_in_a_week =
-  7</tt>;
+  <code>k_</code> followed by words, like <code>const int k_days_in_a_week =
+  7</code>;
 
   - <b>Function Names:</b> Regular functions have mixed case;
   accessors and mutators match the name of the variable. Functions
   should start with a capital letter and have a capital letter for
-  each new word. No underscore, for example <tt> AddTableEntry()</tt>
-  or <tt> DeleteUrl()</tt>.
+  each new word. No underscore, for example <code> AddTableEntry()</code>
+  or <code> DeleteUrl()</code>.
 
   - <b>Namespace Names:</b> Namespace names are all lower-case, and
   based on project names and possibly their directory structure.
   
+  @section emacs Personalizing Emacs
+  
+  I use emacs as the text editor and the following is the part in my
+  <code>.emacs</code> file that corresponds to presonalization for C++:
+
+
+  <code>
+  (setq c-hungry-delete-key t)<br /> 
+  (add-hook 'c-mode-common-hook 'flyspell-prog-mode)<br /> 
+
+  ;; C++ mode style, space only as tab<br /> 
+  (require 'cc-mode)<br /> 
+  (defun my-build-tab-stop-list (width)<br /> 
+  (let ((num-tab-stops (/ 80 width))<br /> 
+  (counter 1)<br /> 
+  (ls nil))<br /> 
+  (while (<= counter num-tab-stops)<br /> 
+  (setq ls (cons (* width counter) ls))<br /> 
+  (setq counter (1+ counter)))<br /> 
+  (set (make-local-variable 'tab-stop-list) (nreverse ls))))<br /> 
+  
+  (defun my-c-mode-common-hook ()<br /> 
+  (setq tab-width 4)<br /> 
+  (my-build-tab-stop-list tab-width)<br /> 
+  (setq c-basic-offset tab-width)<br /> 
+  (setq indent-tabs-mode nil)) ;; force only spaces for indentation<br /> 
+  (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)<br /> 
+
+  ;; get the name of the symbol with C-c C-o<br /> 
+  (c-set-offset 'substatement-open 0)<br /> 
+  (c-set-offset 'case-label '+)<br /> 
+  (c-set-offset 'access-label '-2)<br /> 
+  (c-set-offset 'arglist-cont-nonempty '+)<br /> 
+  (c-set-offset 'arglist-intro '+)<br /> 
+  (c-set-offset 'topmost-intro-cont 0)<br /> 
+  (c-set-offset 'topmost-intro 0)<br /> 
+
+  ;; Doxygen<br /> 
+  (add-hook 'c-mode-common-hook 'doxymacs-mode) <br /> 
+  (defun my-doxymacs-font-lock-hook ()<br /> 
+  (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))<br /> 
+      (doxymacs-font-lock)))<br /> 
+  (add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)<br /> 
+
+  </code>
 */
 
 ////////////////////////////////////////////////////////////
--- include/vesicle.h
+++ include/vesicle.h
@@ -1,20 +1,79 @@
-	
-template <typename ScalarType> class FieldOnSphere
+/// Work space manager
+template <typename ScalarType> class WorkSpaceManager
+{
+  private:
+  public:
+    WorkSpaceManager();
+    ~WorkSpaceManager();
+    int PopWorkMemery();
+    int PushWorkMemery();
+};
+
+/// Cartesian vector
+template <typename FieldType> class D3Vectors
+{
+  private:
+    FieldType *X_,*Y_,*Z_;
+    FieldType *data_;
+  public:
+    D3Vectors();
+    ~D3Vectors();
+    SetData();
+    DotProduct();
+    D3Vectors* CrossProduct(D3Vectors *a, D3Vectors *b);
+    D3Vectors* MatVecProduct(MatType* A, D3Vectors *b);
+    operator+();
+    operator-();
+    operator=();
+};
+
+// Spherical Calculus
+/// Scalar fields defined on the unit sphere.
+template <typename ScalarType> class FieldsOnSphere
 {
 private: 
     int number_of_fields_;
-    int each_field_length_;
+    int number_of_colatitude_grid_;
+    int number_of_longitude_grid_;
+    int fields_length_;
     ScalarType *data_;
-
-public:
-    VectorsOnSphere();
-    VectorsOnSphere(int num_of_fields_in, int each_field_length_in);
-    VectorsOnSphere(int num_of_fields_in, int each_field_length_in, ScalarType *data_in);
-    ~VectorsOnSphere();
     
-    ScalarType* SetData(ScalarType *data_in,int data_length_in); 
-}
+public:
+    FieldsOnSphere();
+    FieldsOnSphere(int num_of_fields_in, int num_colatitude_in, int num_longitude_in);
+    FieldsOnSphere(int num_of_fields_in, int num_colatitude_in, int num_longitude_in, ScalarType *data_in);
+    ~FieldsOnSphere();
+    
+    ScalarType* SetData(ScalarType *data_in, int data_length_in); 
+    ScalarType* GetData();
+};
 
+/// Spherical harmonics
+template <typename FieldsOnSphere> class SphericalHarmonic
+{  
+ private:
+
+    FieldsOnShpere 
+ public:
+    WorkSpaceManager *workVectorStack;
+    int allDerivatives_FrequencyToPoints( VectorType *f, VectorType *Duf, *Dvf, *Duuf,...);
+    int pointsToFrequency(VectorType *fin, VectorType *fout);
+    int frequencyToPoints(VectorType *fin, VectorType *fout);
+    int resample(VecType *fIn, VecType *fout);
+    int resampleWithScaling(VecType *fIn, Real *Uscaling, Real *Vscaling, VecType *fout);
+    
+    // composite functions
+    int allDerivatives_PointsToPoints( VectorType *f, VectorType *Duf, *Dvf, *Duuf,...);
+};
+
+template <typename FieldType> class VectorsOnSphere : public D3Vectors
+{
+private:
+    
+  public:
+    
+};
+/// General Genus Zero Surfaces
 template <typename ScalarType> class Surface
 {
 private:
@@ -27,33 +86,19 @@ public:
     T* X(void);	       
     T* X(T *Xin);    
     
-    vesicle();
-    ~vesicle();
+    Surface();
+    ~Surface();
 
     int SurfGrad(T* vec, T* surf_grad_vec_out);
     int SurdDiv(T* vec, T* surf_div_out);
 };
 
-template <typename VectorType> class Vesicle : public Surface<VectorType>{
+/// Vesicle specific
+template <typename VectorType> class Vesicle : public Surface
+{
     int ApproxCurvatureJacobian(VectorType* vec_in, VectorType* lininearized_curvature_out); 
     int BendingForce(VectorType* position_vector_in, VectorType* bending_force_out);
     int TensileForce(VectorType* tension_in, VectorType* tensile_force_out);
-}
-
-
-template <typename VectorsOnSphere> class SH
-{  
-	
-public:
-    VectorTypeManager *workVectorStack;
-    int allDerivatives_FrequencyToPoints( VectorType *f, VectorType *Duf, *Dvf, *Duuf,...);
-    int pointsToFrequency(VectorType *fin, VectorType *fout);
-    int frequencyToPoints(VectorType *fin, VectorType *fout);
-    int resample(VecType *fIn, VecType *fout);
-    int resampleWithScaling(VecType *fIn, Real *Uscaling, Real *Vscaling, VecType *fout);
-    
-    // composite functions
-    int allDerivatives_PointsToPoints( VectorType *f, VectorType *Duf, *Dvf, *Duuf,...);
 };
 
 int StokesMatVec(Vesicle *ves_in, T* surface_velocity_out);
