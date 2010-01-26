7aead3334c56da47b9904809c0a78b38e780305b
Abtin Rahimian <rahimian@gatech.edu>
2010-01-26 12:57:19 -0500

    arn

--- include/vesicle.h
+++ include/vesicle.h
@@ -11,93 +11,82 @@ template <typename ScalarType> class WorkSpaceManager
 
 template <typename ScalarType> SHScalars
 {
+ private:
+    ScalarType *data_;
  public:
 	int number_of_scalar_Functions_;
 	int p_;
 
-    FieldsOnSphere();
-    FieldsOnSphere(int num_of_fields_in, int num_colatitude_in, int num_longitude_in);
-    FieldsOnSphere(int num_of_fields_in, int num_colatitude_in, int num_longitude_in, ScalarType *data_in);
-    ~FieldsOnSphere();
+    SHScalars();
+    ~SHScalars();
     
     ScalarType* SetData(ScalarType *data_in, int data_length_in); 
-    ScalarType* GetData();
+    ScalarType* GetData(ScalarType *data_out, int data_length);
 };
 	
-template <typename ScalarType> SHVectors
+template <typename ScalarType> SHVectors : public SHScalars
 {
-private:
-  public:
-		FieldType X() data_ ;
-		FieldType Y() data_ + length ; //etc..
-
-    SetData();
-
-		//gb    D3Vectors* MatVecProduct(MatType* A, D3Vectors *b); // remove
-    
+ private:
+ public:
+    friend SHVectors* operator+=(SHVectors *rhs);
+    //friend   operator+(); //gb avoid mallows() problematic for large length
+    //friend   operator-();
+    //friend    operator=();
+    friend CrossProduct(SHVectors *a_in, SHVectors *b_in, SHVectors *aCb_out);
+    friend DotProduct(SHVectors *a_in, SHVectors *b_in, SHScalars *aDb_out);
+    friend axpy(a,x,y,c); //gb ( c = a*x + y )    
 };
-friend    CrossProduct(D3Vectors *aIn, D3Vectors *bIc, D3vectors *aCb);
-//friend   operator+();  //gb avoid mallocs() problematic for large length
-//friend   operator-();
-//friend    operator=();
-friend    DotProduct();
-
-friend		axpy(a,x,y,c);       //gb ( c = a*x + y )
-	
-	
-
-
-    
 
 /// Spherical harmonics
-template <typename FieldsOnSphere> class SphericalHarmonic
+template <typename SHScalars> class SphericalHarmonic
 {  
  private:
-
-    FieldsOnShpere 
  public:
     WorkSpaceManager *workVectorStack;
-    int allDerivatives_FrequencyToPoints( VectorType *f, VectorType *Duf, *Dvf, *Duuf,...);
-    int pointsToFrequency(VectorType *fin, VectorType *fout);
-    int frequencyToPoints(VectorType *fin, VectorType *fout);
-    int resample(VecType *fIn, VecType *fout);
-    int resampleWithScaling(VecType *fIn, Real *Uscaling, Real *Vscaling, VecType *fout);
+    int AllDerivatives_FrequencyToPoints( SHScalars *f_in, SHScalars *Duf_out, SHScalars *Dvf_out, SHScalars *Duuf_out, SHScalars *Duvf_out, SHScalars *Dvvf_out);
+    int PointsToFrequency(SHScalars *f_in, SHScalars *f_out);
+    int FrequencyToPoints(SHScalars *f_in, SHScalars *f_out);
+    int Resample(SHScalars *f_in, SHScalars *f_out);
+    int ResampleWithScaling(SHScalars *f_in, int *p_scaling_in, SHScalars *f_out);
     
     // composite functions
-    int allDerivatives_PointsToPoints( VectorType *f, VectorType *Duf, *Dvf, *Duuf,...);
 };
 
-/// General Genus Zero Surfaces
+/// General Surfaces
 template <typename ScalarType> class Surface
 {
 private:
-    FieldOnSphere<ScalarType> x_, h_, w_, k_, cu_, cv_, normal_;
-    	
+    SHVectors<ScalarType> x_, normal_;
+    SHScalars<ScalarType> h_, w_, k_, cu_, cv_;
+    
 public:
     int p;
-    int number_of_points;
     
-    T* X(void);	       
-    T* X(T *Xin);    
+    //T* X(void);	       arn??
+    //T* X(T *Xin);        arn??
     
     Surface();
     ~Surface();
 
-    int SurfGrad(T* vec, T* surf_grad_vec_out);
-    int SurdDiv(T* vec, T* surf_div_out);
+    int SurfGrad(SHScalars *scalar_in, SHVectors* surf_grad_vec_out);
+    int SurdDiv(SHVectors* vector_in, SHScalars* surf_div_out);
 };
 
 /// Vesicle specific
-template <typename VectorType> class Vesicle : public Surface
+template <typename ScalarType> class Vesicle : public Surface
 {
-    int ApproxCurvatureJacobian(VectorType* vec_in, VectorType* lininearized_curvature_out); 
-    int BendingForce(VectorType* bending_force_out);
-		int BendingForceMatVec(D3Vector in, D3Vector out);
-    int TensileForce(VectorType* tension_in, VectorType* tensile_force_out);
-};
+    public;
+    SHScalars tension_;
+    int ApproxCurvatureJacobian(SHVectors *vec_in, SHScalars *ininearized_curvature_out); 
+    int BendingForce(SHVectors *bending_force_out);
+    int TensileForce(SHVectors *tensile_force_out);
 
-int StokesMatVec(Vesicle *ves_in, T* surface_velocity_out);
-int reparam(Vecicle* ves);
+    int LinearizedBendingOperator(SHScalars *curvature, SHVectors *bending_force_out);
+    int LinearizedTensionOperator(SHScalars *tension  , SHVectors *tensile_force_out);
+    //int BendingForceMatVec(SHVectors vec_in, SHVectors vec_out);
+    
+    friend int StokesMatVec(Vesicle *ves_in,...); //arn?
+};
 
 class StokesMatVec{
  puclic:
--- src/Ves3D.cpp
+++ src/Ves3D.cpp
@@ -1,54 +1,60 @@
 
 
-function timeStepper(vesicle* ves, char* method, options* optsIn)
-{
+// function timeStepper(vesicle* ves, char* method, options* optsIn)
+// {
     
-    T *bendForce;
-    T *rhs1, *rhs2;
-    T *sig;
-    T *tensForce;
-    int p, nv;
-    double ts;
+//     T *bendForce;
+//     T *rhs1, *rhs2;
+//     T *sig;
+//     T *tensForce;
+//     int p, nv;
+//     double ts;
 
 		
     
-    ves.bendingOp(ves.scaGeoProp[1], bendForce);
-    stokesMatVec(p, nv, S.posVec, S.scaGeoProp[2], bendForce, rhs1); 
+//     ves.bendingOp(ves.scaGeoProp[1], bendForce);
+//     stokesMatVec(p, nv, S.posVec, S.scaGeoProp[2], bendForce, rhs1); 
 
-    // Calculating tension
-    GMRES(TENSIONMATVECPOINTER, rhs, sig);
-    S.tensionOp(sig, tensForce);
-    stokesMatVec(p, nv, S.posVec, S.scaGeoProp[2], tensForce, rhs2); 
+//     // Calculating tension
+//     GMRES(TENSIONMATVECPOINTER, rhs, sig);
+//     S.tensionOp(sig, tensForce);
+//     stokesMatVec(p, nv, S.posVec, S.scaGeoProp[2], tensForce, rhs2); 
     
-    ves.posVec = ves.posVec + ts*(rhs1+rhs2); /// assuming overloading
-					      /// the addition
+//     ves.posVec = ves.posVec + ts*(rhs1+rhs2); /// assuming overloading
+// 					      /// the addition
     
-}
+// }
 
 
 function newTimeStep(){
 
-	int nv; // number of vesicles;
+	int nv; // number of vesicles
 	int  p; // discretization size
 	int  m; // number of time steps
 	double dt; 
 	
-	SHVectors vesPnts = InitVesiclesPos(n,p);
-	Surface vesOld=Surface(vesPnts);
-	Surface vesNew=Surface(ves0);
-	SHVectors *force; // allocate memory
+    int vec_size = 6*p*(p+1)*nv;
+	SHVectors ves_pnts = InitVesiclesPos(nv,p);
+	Vesicle ves(ves_pnts);
+	SHVectors force(vec_size);
+    SHVectors work(vec_size), work2(vec_size);
 
-	sm = StokesMatVec();
+    sm = StokesMatVec();
 
-	SHVectors work = SHScalar(vesPnts);
-	
-  for (i=0;i<m;i++){
-		vesOld.BendingForce(force);
-		sm.init(vesOld); 
+    for (i=0;i<m;i++){
+		sm.init(ves); 
+		ves.BendingForce(force);
 		sm.matvec(force,work);
-		axpy(dt,work,vesOld.X(),work);
-		vesNew.X(work);
+ 
+        //Solve for tension;
+        GMRES(LinearizedTensionOperator(),*work, *ves.tension_); //Stokes should be included
+        TensileForce(force);
+        sm.matvec(force,work2);
+        work +=work2;
+        axpy(dt,work,vesOld.X(),work);
+		ves.X(work);
 	}
+}
 
 
 		
