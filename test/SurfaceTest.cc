4cf43b33fa21dbbef63e5ee5736a563820a14ffb
George Biros <biros@gatech.edu>
2010-03-19 13:30:32 -0400

    gb

--- test/SurfaceTest.cc
+++ test/SurfaceTest.cc
@@ -33,14 +33,16 @@ int main(int argc, char ** argv)
     par.rep_up_freq_ = 24;
     par.rep_filter_freq_ = 4;
 
-    // reading data
+    // memory allocation
     Surface<T> S(cpu,par);
     Scalars<T> X(cpu,p,nVec);
     Scalars<T> Y(cpu,p,nVec);
     Scalars<T> Z(cpu,p,nVec);
 
-    myIO.ReadData("../data/eightHump_12.txt",dLen,S.x_.data_);
+    // initializing vesicle positions from text file
+		myIO.ReadData("../data/eightHump_12.txt",dLen,S.x_.data_);
     //myIO.ReadData("../data/dumbbell_cart12_single.txt",dLen,S.x_.data_);
+		// replicate vesicle coordinates
     for(int ii=1;ii<nVec;ii++)
         for(int idx=0;idx<dLen;idx++)
             S.x_.data_[ ii*dLen + idx] = S.x_.data_[idx];
