7fc316ef9775d9dc230cb8659fdd6c1d5378887a
Abtin Rahimian <rahimian@gatech.edu>
2010-06-16 17:15:53 -0400

    ar

--- test/EvolveSurfaceTest.cc
+++ test/EvolveSurfaceTest.cc
@@ -23,14 +23,22 @@ int main(int argc, char **argv)
     Vec x0(nSur, p);
     myIO.ReadData("precomputed/dumbbell_cart12_single.txt",
         x0.size(), x0.begin());
-    
+
+    nSur = 2;
+    x0.resize(nSur);
+    for(int ii=1;ii<nSur; ++ii)
+        x0.getDevice().Memcpy(x0.getSubN(ii), x0.begin(), 
+            x0.getStride() * sizeof(real), MemcpyDeviceToDevice);
+
     //Making the surface, and time stepper
     Sur S(x0);
-    real dt = 1;
-    real T = 100 * dt;    
+    real dt = .5;
+    real T = 1 * dt;    
     EvolveSurface<Sur> Es;
     Es(S, T, dt);
 
     myIO.WriteData("EvolveSurf.txt", S.getPosition().size(), 
         S.getPosition().begin());
+
+    PROFILEREPORT(SortTime);
 }
