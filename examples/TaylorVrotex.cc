d9af7756ff075eba3709ea710cc75ab5d8c1f50e
Abtin <Abtin@lawn-143-215-127-152.lawn.gatech.edu>
2010-08-13 15:01:07 -0400

    ar

--- examples/TaylorVrotex.cc
+++ examples/TaylorVrotex.cc
@@ -1,14 +1,10 @@
-#include "Vectors.h"
-#include "Parameters.h"
-#include "Surface.h"
-#include "VesInteraction.h"
-#include "InterfacialVelocity.h"
-// #include "OperatorsMats.h"
-// #include "EvolveSurface.h"
+#include "EvolveSurface.h"
+#include "CPUKernels.h"
 
-#define DT CPU
-extern const Device<CPU> the_device(0);
-typedef float real;
+#define DT CPU 
+typedef double real;
+
+extern const Device<DT> the_device(0);
 
 int main(int argc, char **argv)
 {
@@ -16,65 +12,62 @@ int main(int argc, char **argv)
         <<"\n ========================"<<endl);
     
     typedef Parameters<real> Par_t;
-    
+    typedef EvolveSurface<real, DT, the_device> Evolve_t;
+    typedef Evolve_t::Vec_t Vec_t;
+
     // Setting the parameters
     Par_t sim_par;
+    sim_par.n_surfs = 2;   
+    sim_par.ts = 1;    
+    sim_par.time_horizon = 2;
+    sim_par.scheme = Explicit;
+    sim_par.bg_flow_param = 0.1;
+    sim_par.rep_maxit = 20;
+    sim_par.save_data = true;    
+    sim_par.save_stride = 1;
+    sim_par.save_file_name = "TaylorVortex.out";
     COUT(sim_par);
     
     //Cleaning the slate
     remove(sim_par.save_file_name.c_str());
 
-    //Types
-    typedef Scalars<real, DT, the_device> Sca_t;
-    typedef Vectors<real, DT, the_device> Vec_t;
 
-    typedef Surface<Sca_t,Vec_t> Sur_t;
-    //typedef VesInteraction<real> Interaction_t;
-    //typedef InterfacialVelocity<Sur_t, Interaction_t> IntVel_t;
+    //Initial vesicle positions 
+    Vec_t x0(sim_par.n_surfs, sim_par.sh_order);
+    
+    //reading the prototype form file
+    DataIO myIO;
+    myIO.ReadData("precomputed/dumbbell_cart12_single.txt",
+        x0, 0, x0.getSubLength());
+    
+    //Making Centers And Populating The Prototype
+    int nVec = sim_par.n_surfs;
+    real* cntrs_host =  new real[nVec * DIM];
+    for(int ii=0; ii<nVec; ++ii)
+    {
+        cntrs_host[DIM*ii    ] = 0;
+        cntrs_host[DIM*ii + 1] = 3*ii;
+        cntrs_host[DIM*ii + 2] = 0;
+    }
+    Array<real, DT, the_device> cntrs(DIM * sim_par.n_surfs);
+    cntrs.getDevice().Memcpy(cntrs.begin(), cntrs_host,
+        cntrs.size() * sizeof(real), MemcpyHostToDevice);
+    Populate(x0, cntrs);
+
+    //Reading Operators From File
+    bool readFromFile = true;
+    Evolve_t::Mats_t Mats(readFromFile, sim_par);
+
+    //Setting the background flow
+    TaylorVortex<Vec_t> vInf(sim_par.bg_flow_param);
    
-    // //IO
-    // DataIO myIO;
-    
-    // //Initializing vesicle positions from text file
-    // Vec_t x0(sim_par.n_surfs, sim_par.sh_order);
-    
-    // //reading the prototype form file
-    // myIO.ReadData("precomputed/dumbbell_cart12_single.txt",
-    //     x0, 0, x0.getSubLength());
-    
-    // //Making Centers And Populating The Prototype
-    // int nVec = sim_par.n_surfs;
-    // real* cntrs_host =  new real[nVec * DIM];
-    // for(int ii=0; ii<nVec; ++ii)
-    // {
-    //     cntrs_host[DIM*ii    ] = 0;
-    //     cntrs_host[DIM*ii + 1] = 3*ii;
-    //     cntrs_host[DIM*ii + 2] = 0;
-    // }
-    // Array<real, DT, the_device> cntrs(DIM * sim_par.n_surfs);
-    // cntrs.getDevice().Memcpy(cntrs.begin(), cntrs_host,
-    //     cntrs.size() * sizeof(real), MemcpyHostToDevice);
-    // Populate(x0, cntrs);
- 
-    // //The Interaction Class
-    // Interaction_t Interaction(NULL);
-
-    // //Reading Operators From File
-    // bool readFromFile = true;
-    // OperatorsMats<Sca_t> Mats(readFromFile, sim_par);
-
-    // //Making The Surface, And Time Stepper
-    // Sur_t S(x0, Mats);
-    // Monitor<Sur_t> M(sim_par);
-    // RepartitionGateway<real> repart(NULL);
-
-    // IntVel_t F(S, Interaction, Mats, sim_par, &TaylorVortex);
-
-//      EvolveSurface<Sur_t, Interaction_t> Es(Mats, sim_par, M, repart);
-   
-//     Es(S, Interaction);
-// }
-    
+    //Finally, Evolve surface
+    PROFILESTART();    
+    Evolve_t Es(sim_par, Mats, x0, &vInf, &StokesAlltoAll);
+    Es.Evolve();    
+    PROFILEEND("",0);
+    PROFILEREPORT(SortFlop);
 }
+
 
 
