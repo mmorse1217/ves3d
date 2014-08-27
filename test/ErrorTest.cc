3f72e47af13236638e1b6a0c81a8ce05b9ab73ee
arahimian <devnull@localhost>
2014-08-27 13:21:57 -0500

    minor bugfix in ErrorTest

--- test/ErrorTest.cc
+++ test/ErrorTest.cc
@@ -6,13 +6,15 @@ using namespace std;
 #ifndef Doxygen_skip
 
 Error_t local_cb_a(const Error_t &err)
-{
+{ 
     COUT("CB_A: Received "<<err<<endl);
+    return err;
 }
 
 Error_t local_cb_b(const Error_t &err)
 {
     COUT("CB_B: Received "<<err<<endl);
+    return err;
 }
 #endif //Doxygen_skip
 
@@ -37,9 +39,9 @@ int main(int argc, char** argv)
 
     int i;
     for(i=0;i<10;++i){
-        if(i=3)
+        if(i==3)
             CHK(SolverDiverged);
-
+	COUT(i<<endl);
         BREAKONERROR();
     }
     COUT("Stopped at i="<<i<<endl);
