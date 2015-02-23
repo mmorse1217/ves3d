9764e8d2e812a3a507e73766d2fe29aa18e7d61d
Abtin Rahimian <arahimian@acm.org>
2015-02-23 11:02:27 -0600

    making bendingForce and linearBendingfoce to do similar computation

--- src/InterfacialForce.cc
+++ src/InterfacialForce.cc
@@ -13,56 +13,53 @@ InterfacialForce<SurfContainer>::~InterfacialForce()
 {}
 
 template<typename SurfContainer>
-void InterfacialForce<SurfContainer>::bendingForce(const SurfContainer &S, 
+void InterfacialForce<SurfContainer>::bendingForce(const SurfContainer &S,
     Vec_t &Fb) const
-{ 
-    Fb.replicate(S.getPosition());
-    s1.replicate(S.getPosition());
-    s2.replicate(S.getPosition());
-
-    S.grad(S.getMeanCurv(), Fb);
-    S.div(Fb, s1);
-    axpy(static_cast<typename SurfContainer::value_type>(-1), s1, s1);
-    
-    xy(S.getMeanCurv(),S.getMeanCurv(), s2);
-    axpy(static_cast<typename SurfContainer::value_type>(-1), 
-        s2, S.getGaussianCurv(), s2);
-    
-    xy(s2, S.getMeanCurv(), s2);
-    axpy(static_cast<typename SurfContainer::value_type>(2), s2, s1, s1);
-    xv(s1, S.getNormal(), Fb);
-
-    axpy(bending_modulus_, Fb, Fb);
-}
-
-template<typename SurfContainer>
-void InterfacialForce<SurfContainer>::linearBendingForce(const SurfContainer &S, 
-    const Vec_t &x_new, Vec_t &Fb) const
-{     
+{
     Fb.replicate(S.getPosition());
     s1.replicate(S.getPosition());
     s2.replicate(S.getPosition());
 
     xy(S.getMeanCurv(), S.getMeanCurv(), s2);
-    axpy(static_cast<typename SurfContainer::value_type>(-1), 
-        S.getGaussianCurv(), s2, s2);
-    
+    axpy(static_cast<typename SurfContainer::value_type>(-1.0),
+         S.getGaussianCurv(), s2, s2);
+    xy(S.getMeanCurv(), s2, s2);
+
+    S.grad(S.getMeanCurv(), Fb);
+    S.div(Fb, s1);
+
+    axpy(static_cast<typename SurfContainer::value_type>(2), s2, s1, s1);
+    xv(s1, S.getNormal(), Fb);
+    axpy(-bending_modulus_, Fb, Fb);
+}
+
+template<typename SurfContainer>
+void InterfacialForce<SurfContainer>::linearBendingForce(const SurfContainer &S,
+    const Vec_t &x_new, Vec_t &Fb) const
+{
+    Fb.replicate(S.getPosition());
+    s1.replicate(S.getPosition());
+    s2.replicate(S.getPosition());
+
     S.linearizedMeanCurv(x_new, s1);
+
+    xy(S.getMeanCurv(), S.getMeanCurv(), s2);
+    axpy(static_cast<typename SurfContainer::value_type>(-1.0),
+        S.getGaussianCurv(), s2, s2);
     xy(s1, s2, s2);
-    
+
     S.grad(s1, Fb);
-    S.div(Fb, s1);    
-    axpy(static_cast<typename SurfContainer::value_type>(2), 
-        s2, s1, s1);
+    S.div(Fb, s1);
+    axpy(static_cast<typename SurfContainer::value_type>(2), s2, s1, s1);
 
     xv(s1, S.getNormal(), Fb);
     axpy(-bending_modulus_, Fb, Fb);
 }
 
 template<typename SurfContainer>
-void InterfacialForce<SurfContainer>::tensileForce(const SurfContainer &S, 
+void InterfacialForce<SurfContainer>::tensileForce(const SurfContainer &S,
     const Sca_t &tension, Vec_t &Fs) const
-{    
+{
     v1.replicate(S.getPosition());
     Fs.replicate(S.getPosition());
 
