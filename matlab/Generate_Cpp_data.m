76fad5f6ce406b8a9cfb246020a84dee270a64ed
Abtin Rahimian <arahimian@acm.org>
2015-02-23 13:39:12 -0600

    making the text files content having 16 digits

--- matlab/Generate_Cpp_data.m
+++ matlab/Generate_Cpp_data.m
@@ -1,15 +1,15 @@
 function Generate_Cpp_data(p, precision, varargin)
-  
+
   for ii = 1:length(varargin)
-    
+
     switch varargin{ii}
      case 'SpHarmRot'
       R = GenerateShRotMats(p);
       R = R(:);
       fileName = ['SpHarmRotMats_p' num2str(p) '_' precision];
-      saveData([fileName '.bin'], R, precision);  
-      save([fileName '.txt'],'R','-ascii'); 
-      
+      saveData([fileName '.bin'], R, precision);
+      save([fileName '.txt'],'R','-ascii','-double');
+
      case 'LegendreTransform'
       [u w] = grule(p+1); u = u(:); u = acos(u);
       v = zeros(size(u));
@@ -24,7 +24,7 @@ function Generate_Cpp_data(p, precision, varargin)
         Yn = Yn*sqrt(2*pi);
         Hn = Hn*sqrt(2*pi);
         Wn = Wn*sqrt(2*pi);
-        
+
         for m = 0:n
           L{1+m}(n+1,:) = w.*Yn(:,n+1+m).';
           LS{1+m}(:,n+1) = Yn(:,n+1+m);
@@ -42,16 +42,16 @@ function Generate_Cpp_data(p, precision, varargin)
       end
 
       fileName = ['legTrans' num2str(p)  '_' precision '.txt'];
-      save(fileName,'Lmat','-ascii'); 
+      save(fileName,'Lmat','-ascii', '-double');
 
       fileName = ['legTransInv' num2str(p)  '_' precision '.txt'];
-      save(fileName,'LImat','-ascii'); 
+      save(fileName,'LImat','-ascii', '-double');
 
       fileName = ['d1legTrans' num2str(p)  '_' precision '.txt'];
-      save(fileName,'Hmat','-ascii'); 
+      save(fileName,'Hmat','-ascii', '-double');
 
       fileName = ['d2legTrans' num2str(p)  '_' precision '.txt'];
-      save(fileName,'Wmat','-ascii'); 
+      save(fileName,'Wmat','-ascii', '-double');
 
 
      case 'DumbbellShape'
@@ -64,13 +64,13 @@ function Generate_Cpp_data(p, precision, varargin)
 
       X = [x(:); y(:); z(:)];
       fileName = ['dumbbell_' num2str(p) '_' precision '.txt'];
-      save(fileName,'X','-ascii'); 
+      save(fileName,'X','-ascii', '-double');
 
      case 'DirectRotation'
       R = GenerateDirectRotMat(p);
       R = R(:);
       fileName = ['all_rot_mats_' num2str(p)  '_' precision '.txt'];
-      save(fileName,'R','-ascii'); 
+      save(fileName,'R','-ascii', '-double');
 
      case 'SingularIntegralWeights'
       sqw = SingularWeights(p);
@@ -79,7 +79,7 @@ function Generate_Cpp_data(p, precision, varargin)
       sqw = sqw';
       sqw = sqw(:);
       fileName = ['sing_quad_weights_' num2str(p)  '_' precision '.txt'];
-      save(fileName,'sqw','-ascii'); 
+      save(fileName,'sqw','-ascii', '-double');
 
      case 'IntegrationWeights'
       [trash qw] = grule(p+1);
@@ -89,7 +89,7 @@ function Generate_Cpp_data(p, precision, varargin)
       u = reshape(u,p+1,[])';
       qw = qw./sin(u(:));
       fileName = ['quad_weights_' num2str(p)  '_' precision '.txt'];
-      save(fileName,'qw','-ascii'); 
+      save(fileName,'qw','-ascii', '-double');
 
      case 'WSpherical'
       u = parDomain(p);
@@ -97,23 +97,23 @@ function Generate_Cpp_data(p, precision, varargin)
       w_sph = sin(u(:));
 
       fileName = ['w_sph_' num2str(p)  '_' precision '.txt'];
-      save(fileName,'w_sph','-ascii'); 
+      save(fileName,'w_sph','-ascii', '-double');
 
-    end      
+    end
   end
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % shape = 'two_ellipse_';
-% 
-% [u,v] = parDomain(p); 
+%
+% [u,v] = parDomain(p);
 % a = 2;
-% X =  sin(u).*cos(v);     
+% X =  sin(u).*cos(v);
 % Y = sin(u).*sin(v);
 % Z = a*cos(u);
-% 
+%
 % cart(1) = d3Vec([X+2;Y;Z-.7]);
 % cart(2) = d3Vec([X-2;Y;Z+.7]);
-% 
+%
 % for ii=1:length(cart)
 %   cart(ii).x = reshape(cart(ii).x,p+1,[])';
 %   cart(ii).x = cart(ii).x(:);
@@ -122,54 +122,54 @@ function Generate_Cpp_data(p, precision, varargin)
 %   cart(ii).z = reshape(cart(ii).z,p+1,[])';
 %   cart(ii).z = cart(ii).z(:);
 % end
-% 
+%
 % X = [cart(1).vecForm; cart(2).vecForm];
 % fileName = [shape num2str(p)];
-% save(fileName,'X','-ascii'); 
+% save(fileName,'X','-ascii', '-double');
 
-    %%% 
+    %%%
 
 % NV = 600;
 % zMax = 80;
 % rMax = 20;
 % minDist = 5;
-% 
+%
 % [x,y,z] = pol2cart(0,0,zMax/2);
 % centers = [x y z];
 % while size(centers,1)<NV
 %   z = rand*zMax;
 %   r = rand*rMax;
 %   t = rand*2*pi;
-%   
+%
 %   [x,y,z] = pol2cart(t,r,z);
 %   d = centers - repmat([x y z],size(centers,1),1);
 %   d = sqrt(min(dot(d,d,2)));
 %   if(d>minDist)
 %     centers = [centers;[x y z]];
 %   end
-%   
+%
 %   disp(size(centers,1));
 % end
 % centers = [centers(:,3) centers(:,2) centers(:,1)];
 % scatter3(centers(:,1),centers(:,2),centers(:,3));
-% 
+%
 % centers = centers';
 % centers = centers(:);
 % fileName = 'centers_tube_600.txt';
-% save(fileName,'centers','-ascii'); 
+% save(fileName,'centers','-ascii', '-double');
 
 % [r t] = meshgrid(0:10:20,0:pi/4:(2*pi-pi/4));
 % r = r(:,2:end); r=r(:);
 % t = t(:,2:end); t=t(:);
 % r = [0;r];
 % t = [0;t];
-% 
+%
 % [x y] = pol2cart(t,r);
 % plot(x,y,'o');
 % c = [zeros(size(x)) y x];
-% 
+%
 % fileName = 'pepperoni.txt';
-% save(fileName,'c','-ascii'); 
+% save(fileName,'c','-ascii', '-double');
 
 % NV = 10;
 % side = 8;
@@ -180,16 +180,16 @@ function Generate_Cpp_data(p, precision, varargin)
 % while size(centers,1)<NV
 %   xx = rand(1,3)*side;
 %   xx(1) = xx(1)/side;
-    
+
 %   d = centers - repmat(xx,size(centers,1),1);
 %   d = sqrt(min(dot(d,d,2)));
 %   if(d>minDist)
 %     centers = [centers;xx];
 %     cc(end+1).x = S.cart.x + xx(1);
 %     cc(end).y = S.cart.y + xx(2);
-%     cc(end).z = S.cart.z + xx(3);    
+%     cc(end).z = S.cart.z + xx(3);
 %   end
-    
+
 %   disp(size(centers,1));
 % end
 % PlotShape(cc,12)
