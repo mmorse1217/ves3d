a0004df2f483797b6bca4c766cc343d170fafaa0
Abtin Rahimian <arahimian@acm.org>
2015-04-21 21:48:37 -0400

    modified shared_ptr so that it works for void type as well

--- include/tr1.h
+++ include/tr1.h
@@ -52,58 +52,66 @@ namespace tr1{
      * span.
      */
     template<typename T>
-    class shared_ptr{
+    class shared_ptr_base{
       public:
         typedef T element_type;
 
-        explicit shared_ptr():
+	explicit shared_ptr_base():
             ptr_(NULL),fore(NULL),aft(NULL) {}
 
-        explicit shared_ptr(T *p):
+        explicit shared_ptr_base(T *p):
             ptr_(p),fore(NULL),aft(NULL) {}
 
-        shared_ptr(shared_ptr const &r):
+        shared_ptr_base(shared_ptr_base const &r):
             ptr_(r.ptr_)
         { join(&r); }
 
-        shared_ptr& operator=(shared_ptr const &r){
+        shared_ptr_base& operator=(shared_ptr_base const &r){
             leave();
             join(&r);
             ptr_ = r.ptr_;
 	    return *this;
         }
 
-        shared_ptr& operator=(T* p){
+        const shared_ptr_base& operator=(shared_ptr_base const &r) const{
+            leave();
+            join(&r);
+            ptr_ = r.ptr_;
+	    return *this;
+        }
+
+        shared_ptr_base& operator=(T* p){
             leave();
             ptr_ = p;
 	    return *this;
         }
 
-        //
-        ~shared_ptr()
+        const shared_ptr_base& operator=(T* p) const{
+            leave();
+            ptr_ = p;
+	    return *this;
+        }
+
+        virtual ~shared_ptr_base()
         {
             COUTDEBUG("Destructing shared_ptr to "<<this->ptr_);
             leave();
         }
 
-        //
-        T& operator*() {return *ptr_;}
         T* operator->() {return ptr_;}
+        const T* operator->() const {return ptr_;}
+	T* get() {return ptr_;}
+	const T* get() const {return ptr_;}
 
-        T& operator*() const {return *ptr_;}
-        T* operator->() const {return ptr_;}
-
-        T* get() const {return ptr_;}
-
-        operator bool() const
+	operator bool() const
         { return(bool(ptr_)); }
 
-      private:
-        T* ptr_;
-        mutable shared_ptr const* fore;
-        mutable shared_ptr const* aft;
+      protected:
+        mutable T* ptr_;
+        mutable shared_ptr_base const* fore;
+        mutable shared_ptr_base const* aft;
 
-        void join(shared_ptr const* before) const{
+	void join(shared_ptr_base const* before) const{
             fore      = before;
             aft       = before->aft;
             fore->aft = this;
@@ -112,17 +120,82 @@ namespace tr1{
                 aft->fore = this;
         }
 
-        void leave() const{
+	void leave() const{
             if (fore)
                 fore->aft = aft;
 
             if (aft)
                 aft->fore = fore;
 
-            if (fore == NULL && aft == NULL && ptr_){
+            if ( is_last() && ptr_){
                 COUTDEBUG("Deleting pointer to "<<ptr_);
-                delete ptr_;
-            }
+		delete ptr_;
+            } else {
+		ptr_ = NULL;
+	    }
+        }
+
+	bool is_last() const{
+	    return (fore == NULL && aft == NULL);
+	}
+    };
+
+    template<typename T>
+    class shared_ptr : public shared_ptr_base<T>{
+      public:
+	explicit shared_ptr(){};
+        explicit shared_ptr(T *p) : shared_ptr_base<T>(p) {}
+	shared_ptr(shared_ptr const &r) : shared_ptr_base<T>(r) {}
+
+        shared_ptr& operator=(shared_ptr const &r){
+	    shared_ptr_base<T>::operator=(r);
+	    return *this;
+        }
+
+	const shared_ptr& operator=(shared_ptr const &r) const{
+	    shared_ptr_base<T>::operator=(r);
+	    return *this;
+        }
+
+        shared_ptr& operator=(T* p){
+	    shared_ptr_base<T>::operator=(p);
+	    return *this;
+        }
+
+        const shared_ptr& operator=(T* p) const{
+	    shared_ptr_base<T>::operator=(p);
+	    return *this;
+        }
+
+        T& operator*() {return *shared_ptr_base<T>::ptr_;}
+	const T& operator*() const {return *shared_ptr_base<T>::ptr_;}
+    };
+
+    template<>
+    class shared_ptr<void> : public shared_ptr_base<void>{
+      public:
+	explicit shared_ptr(){};
+        explicit shared_ptr(void *p) : shared_ptr_base<void>(p) {}
+	shared_ptr(shared_ptr const &r) : shared_ptr_base<void>(r) {}
+
+        shared_ptr& operator=(shared_ptr const &r){
+	    shared_ptr_base<void>::operator=(r);
+	    return *this;
+        }
+
+        const shared_ptr& operator=(shared_ptr const &r) const{
+	    shared_ptr_base<void>::operator=(r);
+	    return *this;
+        }
+
+        shared_ptr& operator=(void* p){
+	    shared_ptr_base<void>::operator=(p);
+	    return *this;
+        }
+
+        const shared_ptr& operator=(void* p) const{
+	    shared_ptr_base<void>::operator=(p);
+	    return *this;
         }
     };
 };
