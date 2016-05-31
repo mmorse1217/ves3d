32cdc4015977ddde27ddbccb27e6030a641d5e57
Abtin Rahimian <arahimian@acm.org>
2016-05-31 10:45:45 -0400

    removing deprecated benchmarking code

--- include/InterfacialVelocity.h
+++ include/InterfacialVelocity.h
@@ -140,28 +140,6 @@ class InterfacialVelocity
     mutable int checked_out_work_vec_;
 
     void purgeTheWorkSpace() const;
-
-  //////////////////////////////////////////////////////////////////////////
-  /// DEBUG mode methods ///////////////////////////////////////////////////
-  //////////////////////////////////////////////////////////////////////////
-#ifndef NDEBUG
-  public:
-    bool benchmarkExplicit(Vec_t &Fb, Vec_t &SFb, Vec_t &vel,
-        Sca_t &tension, Vec_t &xnew, value_type tol);
-
-    bool benchmarkImplicit(Sca_t &tension, Vec_t &matvec,
-        Vec_t &xnew, value_type tol);
-
-    bool benchmarkBendingForce(const Vec_t &x, Vec_t &Fb, value_type tol) const;
-    bool benchmarkStokes(const Vec_t &F, Vec_t &SF, value_type tol) const;
-    bool benchmarkBgFlow(const Vec_t &SFb, Vec_t &vel, value_type tol) const;
-    bool benchmarkTension(const Vec_t &vel, Sca_t &tension, value_type tol) const;
-    bool benchmarkNewPostitionExplicit(Vec_t &xnew, value_type tol);
-
-    bool benchmarkTensionImplicit(Sca_t &tension, value_type tol);
-    bool benchmarkMatVecImplicit(const Vec_t &x, Vec_t &matvec, value_type tol);
-    bool benchmarkNewPostitionImplicit(Vec_t &xnew, value_type tol);
-#endif //NDEBUG
 };
 
 #include "InterfacialVelocity.cc"
